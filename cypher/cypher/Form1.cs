508968520376e606619bf9d6a1faf2a210138aad
Antonio Hohnjec <antonio.hohnjec@fer.hr>
2024-10-01 13:39:18 +0200

    Copying files, making backup from disk to github.

---
+++ cypher/cypher/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cypher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string codiraj(char x)
+        {
+            switch (x)
+            {
+                case 'a':
+                    return ".-";
+                case 'b':
+                    return "-...";
+                case 'c':
+                    return "-.-.";
+                case 'd':
+                    return "-..";
+                case 'e':
+                    return ".";
+                case 'f':
+                    return "..-.";
+                case 'g':
+                    return "--.";
+                case 'h':
+                    return "....";
+                case 'i':
+                    return "..";
+                case 'j':
+                    return ".---";
+                case 'k':
+                    return "-.-";
+                case 'l':
+                    return ".-..";
+                case 'm':
+                    return "--";
+                case 'n':
+                    return "-.";
+                case 'o':
+                    return "---";
+                case 'p':
+                    return ".--.";
+                case 'q':
+                    return "--.-";
+                case 'r':
+                    return ".-.";
+                case 's':
+                    return "...";
+                case 't':
+                    return "-";
+                case 'u':
+                    return "..-";
+                case 'v':
+                    return "...-";
+                case 'w':
+                    return ".--";
+                case 'x':
+                    return "-..-";
+                case 'y':
+                    return "-.--";
+                case 'z':
+                    return "--..";
+                default:
+                    return "";
+            }
+        }
+
+        public string spoji(string a)
+        {
+            string rj = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                rj += codiraj(a[i]);
+            }
+
+            return rj;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = spoji(textBox1.Text);
+        }
+    }
+}
